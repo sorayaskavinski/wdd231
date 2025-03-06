86204d34061d3f3cf26188058ae76b9f41b53c3e
sorayaskavinski <soraya1br@gmail.com>
2025-03-06 18:38:50 -0300

    ..

--- scripts/base.js
+++ scripts/base.js
@@ -1,11 +1,9 @@
 document.getElementById("year").textContent = new Date().getFullYear();
-
-document.getElementById("lastModified").textContent = "Last Updated: " + document.lastModified;
-
-const menuToggle = document.querySelector(".menu-toggle");
-  const navLinks = document.querySelector(".nav-links");
-
-  menuToggle.addEventListener("click", () => {
-    navLinks.classList.toggle("active");
-  });
-
+        document.getElementById("lastModified").textContent = "Last Updated: " + document.lastModified;
+        
+        const menuToggle = document.querySelector(".menu-toggle");
+        const navLinks = document.querySelector(".nav-links");
+        
+        menuToggle.addEventListener("click", () => {
+            navLinks.classList.toggle("active");
+        });
