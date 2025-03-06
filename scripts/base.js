821e2736d972b98ddeae36d07c03989c62879c61
sorayaskavinski <soraya1br@gmail.com>
2025-03-05 21:25:51 -0300

    first commit

---
+++ scripts/base.js
@@ -0,0 +1,18 @@
+document.getElementById('year').textContent = new Date().getFullYear();
+
+const lastModifiedDate = new Date(document.lastModified);
+document.getElementById('lastModified').textContent =
+    `Last Updated: ${lastModifiedDate.toLocaleDateString('en-US', {
+        year: 'numeric',
+        month: 'long',
+        day: 'numeric',
+    })}`;
+
+const hamburger = document.getElementById('hamburger');
+const navMenu = document.querySelector('nav ul');
+
+hamburger.addEventListener('click', () => {
+    const isVisible = navMenu.classList.toggle('visible');
+    hamburger.setAttribute('aria-expanded', isVisible.toString());
+    hamburger.textContent = isVisible ? '✖' : '☰';
+});
